f5f1d5f505c3f8eba41b9cd12e107224dffc697b
gaurav08suri <gaurav08suri@gmail.com>
2021-06-22 19:11:50 +0530

    Added score columns

---
+++ src/main/resources/db/migration/V4__add_score_columns.sql
@@ -0,0 +1,5 @@
+alter table registration_app.participants
+add column level1_score varchar(100),
+add column level2_score varchar(100),
+add column level3_score varchar(100),
+add column level4_score varchar(100);
